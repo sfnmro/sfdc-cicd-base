7843a645dd93360991618d0e34bd0ce99eb5426d
Stefano Moro <stmoro@deloitte.it>
2022-04-24 11:54:52 +0200

    Added Log

--- .sfdy-plugins/metadata-fix.js
+++ .sfdy-plugins/metadata-fix.js
@@ -1,4 +1,5 @@
 module.exports = async ({ environment, log }, helpers) => {
+  log('Metadata Fix for ENV: ' + environment)
   helpers.xmlTransformer('connectedApps/*', async (filename, fJson) => {
     delete fJson['oauthConfig']
     log('ConnectedApps fix complete')
